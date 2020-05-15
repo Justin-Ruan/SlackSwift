dc3567df92ead5d201c26d62ca33cbc9bd194a1c
Charles Ku <34418539+charlesKu915@users.noreply.github.com>
2020-05-15 18:19:26 +0800

    Update Package.swift

--- Package.swift
+++ Package.swift
@@ -12,7 +12,7 @@ let package = Package(
     ],
     dependencies: [
         // 💧 A server-side Swift web framework.
-        .package(name: "Vapor", url: "https://github.com/vapor/vapor.git", from: "3.0.0")
+        .package(url: "https://github.com/vapor/vapor.git", from: "3.0.0")
     ],
     targets: [
         // Targets are the basic building blocks of a package. A target can define a module or a test suite.

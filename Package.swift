da2583c62805d6d8c0cd791283a0cb28b112a019
Justin Ruan <justin.ruan@cardinalblue.com>
2021-09-12 14:04:32 +0800

    Update swfit tool to 5.2

--- Package.swift
+++ Package.swift
@@ -1,4 +1,4 @@
-// swift-tools-version:5.1.0
+// swift-tools-version:5.2.0
 // The swift-tools-version declares the minimum version of Swift required to build this package.
 
 import PackageDescription
@@ -18,7 +18,7 @@ let package = Package(
     targets: [
         // Targets are the basic building blocks of a package. A target can define a module or a test suite.
         // Targets can depend on other targets in this package, and on products in packages which this package depends on.
-        .target(name: "SlackSwift", dependencies: ["Vapor"]),
+        .target(name: "SlackSwift", dependencies: [.product(name: "Vapor", package: "vapor")]),
         .testTarget(name: "SlackSwiftTests", dependencies: ["SlackSwift"]),
     ]
 )
